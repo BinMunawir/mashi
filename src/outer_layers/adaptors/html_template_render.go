62dc91ab349f8ff3fd0a0012089622e4dc4e08d4
BinMunawir <a.alrashidi@sary.com>
2022-10-25 19:10:55 +0300

    impl: render html template

---
+++ src/outer_layers/adaptors/html_template_render.go
@@ -0,0 +1,18 @@
+package html_template_render
+
+import (
+	"bytes"
+	"html/template"
+	"log"
+)
+
+func HTMLRender(data map[string]string) []byte {
+	tmpl, err := template.ParseFiles("templates/welcome_report.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var buf bytes.Buffer
+	tmpl.Execute(&buf, data)
+
+	return buf.Bytes()
+}
