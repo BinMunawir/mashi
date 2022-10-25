f823e34c2cd0c563fb5e0e293960e4076ef0f17f
BinMunawir <a.alrashidi@sary.com>
2022-10-25 19:11:43 +0300

    impl: return html from endpoint

--- src/outer_layers/infrastructure/web/routes.go
+++ src/outer_layers/infrastructure/web/routes.go
@@ -1,17 +1,18 @@
 package web
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/BinMunawir/mashi/src/inner_layers/usecases/general"
+	html_template_render "github.com/BinMunawir/mashi/src/outer_layers/adaptors"
 )
 
 func RegisterRoutes() {
 	http.HandleFunc("/api/welcome_report", func(w http.ResponseWriter, r *http.Request) {
 		data := general.GenerateWelcomReport()
-		json.NewEncoder(w).Encode(data)
+		html := html_template_render.HTMLRender(data)
+		w.Write(html)
 	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello from Mashi")
