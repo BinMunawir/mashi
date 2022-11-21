ac14ad6d906aba34552460035eb8788d2ef96bfc
BinMunawir <a.alrashidi@sary.com>
2022-11-21 05:07:25 +0300

    cleanup main

--- main.go
+++ main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/BinMunawir/mashi/src/core/services"
+	"github.com/BinMunawir/mashi/src/delivery/infra/web"
 )
 
 func main() {
 	fmt.Println("Hello from Mashi")
-	// web.InitializeHTTPServer()
-	// services.NewKafka().Produce([]byte("hi"), "demo")
-	services.NewKafka().Consume("demo")
-
+	web.InitializeHTTPServer()
 }
