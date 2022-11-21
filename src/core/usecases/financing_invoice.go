84bf999af5a64b972b73153437fd404dec9fac64
BinMunawir <a.alrashidi@sary.com>
2022-11-21 05:49:57 +0300

    impl: trigger event message after financing an invoice

--- src/core/usecases/financing_invoice.go
+++ src/core/usecases/financing_invoice.go
@@ -1,6 +1,10 @@
 package usecases
 
+import "encoding/json"
+
 func FinancingInvoice(invoiceDTO map[string]interface{}) error {
 	invoiceRepository.SaveInvoice(invoiceDTO)
+	invoiceByte, _ := json.Marshal(invoiceDTO)
+	messageBus.Produce(invoiceByte, "invoice_financed")
 	return nil
 }
--- src/core/usecases/init.go
+++ src/core/usecases/init.go
@@ -1,9 +1,14 @@
 package usecases
 
-import "github.com/BinMunawir/mashi/src/core/repositories"
+import (
+	"github.com/BinMunawir/mashi/src/core/repositories"
+	"github.com/BinMunawir/mashi/src/core/services"
+)
 
 var invoiceRepository repositories.InvoiceRepository
+var messageBus services.MessageBus
 
-func Init(_invoiceRepository repositories.InvoiceRepository) {
+func Init(_invoiceRepository repositories.InvoiceRepository, _messageBus services.MessageBus) {
 	invoiceRepository = _invoiceRepository
+	messageBus = _messageBus
 }
